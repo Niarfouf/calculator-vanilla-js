d36fca41ac316cb000756a56ea27fdfe6236738a
Niarfouf <tniarfeix@gmail.com>
2023-11-02 15:28:42 +0100

    Add: eventListener functions to buttons

--- script.js
+++ script.js
@@ -1,12 +1,89 @@
 let firstNumber
 let secondNumber
 let operator
-let display
+let display = 0
+let displayControl = 0
 const displayDiv = document.querySelector(".display")
-const buttonsNumber = document.querySelectorAll(".number")
-const buttonsOperator = document.querySelectorAll(".operator")
-const buttonResult = document.querySelector(".result")
-const buttonClear = document.querySelector(".clear")
+const numberButtons = document.querySelectorAll(".number")
+const operatorButtons = document.querySelectorAll(".operator")
+const resultButton = document.querySelector(".result")
+const clearButton = document.querySelector(".clear")
+const undoButton = document.querySelector(".undo")
+const decimalButton = document.querySelector(".decimal")
+decimalButton.addEventListener("click", function() {
+    let displayString = displayDiv.textContent
+    if (displayString.indexOf(".") === -1) {
+        displayString += "."
+        displayDiv.textContent = displayString
+        display = displayString
+    }
+})
+undoButton.addEventListener("click", function() { 
+    let displayArray = displayDiv.textContent.split("")
+    displayArray.splice(displayArray.length - 1, 1)
+    display = displayArray.join("")
+    displayDiv.textContent = display
+})
+clearButton.addEventListener("click", function() {
+    firstNumber = ""
+    secondNumber = ""
+    operator = ""
+    display = 0
+    displayControl = 0
+    displayDiv.textContent = display
+})
+numberButtons.forEach(button => {
+    button.addEventListener("click", function() {
+    
+        if (displayControl) {
+            display = 0
+            displayControl = 0  
+        }
+        
+        display += button.value
+        display = Number(display)
+        displayDiv.textContent = display
+    })
+})
+
+operatorButtons.forEach(button => {
+    button.addEventListener("click", function() {
+        if (operator) {
+            secondNumber = Number(display)
+            display = operate(firstNumber, secondNumber, operator)
+            display = Math.round(display * 100000) / 100000
+            if (display === Infinity) {
+                displayDiv.textContent = "you tried the impossible divide by zero you fool"
+            }
+            else {
+                displayDiv.textContent = display
+            }
+                    
+        } 
+        firstNumber = Number(display)
+        operator = button.value
+        displayControl = 1
+        
+    })
+})
+
+resultButton.addEventListener("click", function() {
+    if (operator) {
+        secondNumber = Number(display)
+        display = operate(firstNumber, secondNumber, operator)
+        display = Math.round(display * 100000) / 100000
+        if (display === Infinity) {
+            displayDiv.textContent = "you tried the impossible divide by zero you fool"
+        }
+        else {
+            displayDiv.textContent = display
+        }
+        
+        operator = ""
+    } 
+    displayControl = 1
+    
+})
 
 function add(x, y) {
     return x + y
@@ -42,3 +119,4 @@ function operate(x, y, operator) {
     }
     return result
 }
+
